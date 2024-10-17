0ed2a3bf5cadcc2b2a1cd39a8b8148132a9a0406
Antypurus <tiagojsmagalhaes@gmail.com>
2024-10-17 03:16:16 +0100

    added prototype of stack frame printing for windows

--- ShadeTech/ShadeTech.cpp
+++ ShadeTech/ShadeTech.cpp
@@ -3,12 +3,81 @@ import core;
 #include "Log.h"
 #include "assert.h"
 
+#include <Windows.h>
+#include <dbghelp.h>
+
 using namespace SHD::Rendering::RHI::Vulkan;
 
 int main()
 {
     LOG_SUCCESS("Welcome To the SDH Network Agent");
-    ASSERT(0==0, "something");
+
+#if PLATFORM_WINDOWS
+    auto process = GetCurrentProcess();
+    auto thread = GetCurrentThread();
+
+    CONTEXT context;
+    RtlCaptureContext(&context);
+
+    STACKFRAME64 stackframe = {
+        .AddrPC = {
+            .Offset = context.Rip,
+            .Mode = AddrModeFlat,
+        },
+        .AddrFrame = {
+            .Offset = context.Rbp,
+            .Mode = AddrModeFlat,
+        },
+        .AddrStack = {
+            .Offset = context.Rsp,
+            .Mode = AddrModeFlat,
+        },
+    };
+
+    SymInitialize(process, NULL, TRUE);
+
+    while (true) {
+        if (!StackWalk64(IMAGE_FILE_MACHINE_AMD64,
+                         process,
+                         thread,
+                         &stackframe,
+                         &context,
+                         nullptr,
+                         SymFunctionTableAccess64,
+                         SymGetModuleBase64,
+                         nullptr)) {
+            // end of stack reached
+            break;
+        }
+
+        auto address = stackframe.AddrPC.Offset;
+        if (address == 0)
+            break;
+
+        char symbolBuffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+        auto* symbol = (SYMBOL_INFO*)(void*)symbolBuffer;
+        symbol->MaxNameLen = MAX_SYM_NAME;
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+        auto displacement = 0ULL;
+        if (SymFromAddr(process, address, &displacement, symbol)) {
+            LOG_INFO("Name:%s", (char*)symbol->Name);
+
+            // line info
+            IMAGEHLP_LINE64 line = {
+                .SizeOfStruct = sizeof(IMAGEHLP_LINE64),
+            };
+            auto lineDisplacement = 0UL;
+            if (SymGetLineFromAddr64(process, address, &lineDisplacement, &line)) {
+                LOG_INFO("\t Line: %ld, File: %s", line.LineNumber, line.FileName);
+            }
+        } else {
+            LOG_INFO("Missing Symbol At Address: %lld", address);
+        }
+    }
+
+    SymCleanup(process);
+#endif
 
     return 0;
 }
