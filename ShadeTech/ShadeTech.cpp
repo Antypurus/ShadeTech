d7bc4bb2212f169127fadc3d50e4db2c4d5fbd83
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2024-10-22 04:38:06 +0100

    macos specific debug symbol handling

--- ShadeTech/ShadeTech.cpp
+++ ShadeTech/ShadeTech.cpp
@@ -8,11 +8,11 @@ import core;
 #include <dbghelp.h>
 #else
 #include <dlfcn.h>
-#include <execinfo.h>
-#include <libdwarf.h>
 #include <dwarf.h>
-#include <libunwind.h>
+#include <execinfo.h>
 #include <fcntl.h>
+#include <libdwarf.h>
+#include <libunwind.h>
 #include <unistd.h>
 #endif
 
@@ -84,8 +84,6 @@ int main(int argc, char** argv)
 
     SymCleanup(process);
 #else
-    std::cout << argv[0] << std::endl;
-
     unw_cursor_t cursor;
     unw_context_t context;
 
@@ -97,30 +95,30 @@ int main(int argc, char** argv)
         unw_get_reg(&cursor, UNW_REG_IP, &ip);
         unw_get_proc_name(&cursor, nullptr, 0, &offset);
 
-
 #if PLATFORM_LINUX
-        int fd = open("/proc/self/exe", O_RDONLY);
-#elif PLATFORM_MACOS
-        int fd = open(argv[0], O_RDONLY);
+        std::string path = "/proc/self/exe";
+#else
+        std::string path = std::string(argv[0]) + ".dSYM/Contents/Resources/DWARF/" + std::string(argv[0]);
 #endif
-        if(fd < 0)
-        {
+        int fd = open(path.c_str(), O_RDONLY);
+        if (fd < 0) {
             std::cout << "Failed to open process file" << std::endl;
         }
 
         Dwarf_Debug dbg = nullptr;
         Dwarf_Error err;
         auto ret = dwarf_init_b(fd, DW_GROUPNUMBER_ANY, nullptr, nullptr, &dbg, &err);
-        if(ret == DW_DLV_ERROR) {
-            std::cout << "failed to init libdwarf based on current process:"
-                << dwarf_errmsg(err)
-                << std::endl;;
-        }
-        else if(ret == DW_DLV_NO_ENTRY) {
+        if (ret == DW_DLV_ERROR) {
+            std::cout << "failed to init libdwarf based on current process:" << dwarf_errmsg(err) << std::endl;
+            ;
+        } else if (ret == DW_DLV_NO_ENTRY) {
             std::cout << "no entry error" << std::endl;
         }
+        else{
+            LOG_SUCCESS("libdwarf initialized");
+        }
 
-    }while (unw_step(&cursor) > 0);
+    } while (unw_step(&cursor) > 0);
 
 #endif
 
