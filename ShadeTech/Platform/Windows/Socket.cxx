d8266fe067afcff6b1b4286065823fc54430ceba
Antypurus <tiagojsmagalhaes@gmail.com>
2024-11-22 23:48:18 +0000

    prototyping windows networking

---
+++ ShadeTech/Platform/Windows/Socket.cxx
@@ -0,0 +1,42 @@
+module;
+
+#include "Platform.h"
+#if PLATFORM_WINDOWS
+
+#include "assert.h"
+
+#include <WS2tcpip.h>
+#include <winsock2.h>
+
+export module socket;
+namespace SHD {
+namespace Windows {
+
+export class TCPSocket
+{
+private:
+public:
+    TCPSocket()
+    {
+        WSADATA wsaData;
+        int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        ASSERT(result == 0, "Failed to initialize Window Networking (Winsock2)");
+        LOG_SUCCESS("Windows Networking Initialized");
+    }
+};
+
+}
+}
+
+#else
+
+export module socket;
+
+namespace SHD {
+namespace Windows {
+export class TCPSocket
+{};
+}
+}
+
+#endif
