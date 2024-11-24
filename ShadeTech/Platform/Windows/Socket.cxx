218171ed4b3db90e0a0b593e692bbafedabd6bc5
Antypurus <tiagojsmagalhaes@gmail.com>
2024-11-24 05:03:48 +0000

    working on socket prototyping

--- ShadeTech/Platform/Windows/Socket.cxx
+++ ShadeTech/Platform/Windows/Socket.cxx
@@ -22,6 +22,21 @@ public:
         int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
         ASSERT(result == 0, "Failed to initialize Window Networking (Winsock2)");
         LOG_SUCCESS("Windows Networking Initialized");
+
+        addrinfo* resultInfo;
+        addrinfo hints;
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_flags = AI_PASSIVE;
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        result = getaddrinfo(NULL, "8080", &hints, &resultInfo);
+        ASSERT(result == 0, "Failed to get address info");
+
+        SOCKET server_socket = socket(resultInfo->ai_family, resultInfo->ai_socktype, resultInfo->ai_protocol);
+        ASSERT(server_socket != INVALID_SOCKET, "Failed to create server socket");
+        LOG_SUCCESS("Server Socket Created");
     }
 };
 
