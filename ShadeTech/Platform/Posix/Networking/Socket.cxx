cb78d7f2e3b61601212359402dd2eb97857d8910
Antypurus <tiagojsmagalhaes@gmail.com>
2025-01-03 11:18:29 +0000

    prototyping posix tcp server sockets

--- ShadeTech/Platform/Posix/Networking/Socket.cxx
+++ ShadeTech/Platform/Posix/Networking/Socket.cxx
@@ -4,17 +4,91 @@ module;
 
 #if PLATFORM_POSIX
 
-#include <sys/socket.h>
+#include "Log.h"
+
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
 
 export module posix.socket;
-namespace SHD::POSIX::Networking
-{
+namespace SHD::POSIX::Networking {
 
-    export class TCPServer
+export class TCPConnectionSocket
+{
+private:
+    int m_socket = -1;
+
+public:
+    TCPConnectionSocket(int socket_fd) :
+        m_socket(socket_fd) {};
+
+    TCPConnectionSocket(TCPConnectionSocket&& other);
+    TCPConnectionSocket& operator=(TCPConnectionSocket&& other);
+
+    ~TCPConnectionSocket()
     {
-    public:
-    };
+        if (this->m_socket != -1) {
+            close(this->m_socket);
+            this->m_socket = -1;
+        }
+    }
+
+public:
+    TCPConnectionSocket(const TCPConnectionSocket& other) = delete;
+    TCPConnectionSocket& operator=(const TCPConnectionSocket& other) = delete;
+};
+
+export class TCPServerSocket
+{
+private:
+    int m_socket = -1;
+
+public:
+    TCPServerSocket()
+    {
+        this->m_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+        sockaddr_in address = {};
+        address.sin_family = AF_INET;
+        address.sin_port = htons(8080);
+        address.sin_addr.s_addr = INADDR_ANY;
+
+        auto result = bind(this->m_socket, (sockaddr*)&address, sizeof(address));
+        if (result < 0) {
+            LOG_ERROR("Failed to bind server socket");
+            return;
+        }
+
+        result = listen(this->m_socket, 1);
+        if (result < 0) {
+            LOG_ERROR("Failed to listen for connectios");
+            return;
+        }
+
+        auto connection_socket = accept(this->m_socket, NULL, NULL);
+        if (connection_socket < 0) {
+            LOG_ERROR("Failed to accept inbound connection");
+            return;
+        }
+
+        char buffer[1500] = { 0 };
+        while (recv(connection_socket, (char*)buffer, 1500, 0)) {
+            LOG_INFO("%s", (char*)buffer);
+        }
+
+        close(connection_socket);
+    }
+
+    ~TCPServerSocket()
+    {
+        if (this->m_socket != -1) {
+            close(this->m_socket);
+            this->m_socket = -1;
+        }
+    }
+};
 
 }
 
--- ShadeTech/ShadeTech.cxx
+++ ShadeTech/ShadeTech.cxx
@@ -2,14 +2,25 @@ module;
 
 #include "Types.h"
 
+import posix.socket;
+import Application;
+
 export module ShadeTech;
 
-export import Application;
 export class ShadeTech : public SHD::Application
 {
 public:
-    int Run(u32 argument_count, char** arguments) override { 
+<<<<<<< HEAD
+    int Run(u32 argument_count, char** arguments) override {
+        using namespace SHD::POSIX::Networking; 
+        TCPServer server;
         return 0; 
+=======
+    int Run(u32 argument_count, char** arguments) override
+    {
+        using namespace SHD::POSIX::Networking;
+        TCPServerSocket server;
+        return 0;
+>>>>>>> 7aa13bc545f1e456887dd9a0c87428a14201dfdc
     }
 };
-
