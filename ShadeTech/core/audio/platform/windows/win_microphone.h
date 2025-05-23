cc1f8f61d6c2552877ffd8c78d366f7f2b5e7852
Antypurus <tiagojsmagalhaes@gmail.com>
2025-05-23 03:35:21 +0100

    added audio sample placeholder structure

--- ShadeTech/core/audio/platform/windows/win_microphone.h
+++ ShadeTech/core/audio/platform/windows/win_microphone.h
@@ -1,10 +1,25 @@
 #pragma once
 
-namespace SHD::Audio::Windows
+#include <core/types.h>
+#include <core/utils/ring_buffer.h>
+
+namespace SHD::Audio::Windows {
+
+struct audio_sample
 {
-    struct Microphone
-    {
-    public:
-        Microphone();
-    };
+    float channel_0 = 0;
+    float channel_1 = 0;
+    float channel_2 = 0;
+    float channel_3 = 0;
+    u8 channel_count = 1;
+};
+
+struct Microphone
+{
+private:
+    ring_buffer<audio_sample> m_audio_samples;
+
+public:
+    Microphone();
+};
 }
